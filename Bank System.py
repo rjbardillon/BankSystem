f3dcebc542545097c9f80e08fbbb434f28523b26
rjbardillon <62207880+rjbardillon@users.noreply.github.com>
2021-05-31 11:24:43 +0800

    Update

--- Bank System.py
+++ Bank System.py
@@ -2,3 +2,5 @@ print("Group Members\n"
       "Bardillon, Romeo Jr. M.\n"
       "Porlares, Aaron S.\n"
       "Rivera, Rhodemil Zeth M.")
+name = input("What is your name? ")
+print(f"Welcome {name}")
