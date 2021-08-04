636e8bc55f2ef6704714c0351953e8f88f0a6215
rjbardillon <62207880+rjbardillon@users.noreply.github.com>
2021-08-04 11:34:26 +0800

    Updated

--- BankWithDatabase/demo.py
+++ BankWithDatabase/demo.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="admin1234", database="atmsystem")
+mydb = mysql.connector.connect(host="192.168.1.5", user="root", passwd="admin1234", database="atmsystem")
 mycursor = mydb.cursor()
 
-mycursor.execute("insert into customer values('Romsky Jr M Bardillon', 5678, 10000)")
+# mycursor.execute("insert into customer values('Romsky Jr M Bardillon', 5678, 10000)")
 mycursor.execute("select * from customer")
 
 for i in mycursor:
